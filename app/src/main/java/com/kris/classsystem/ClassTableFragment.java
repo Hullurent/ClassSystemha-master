84de8b3bc1fc598e12d204088b3df0b56835929a
hullurent <hullurent@hullurent.local>
2019-06-13 14:20:47 +0800

    Initial commit

---
+++ app/src/main/java/com/kris/classsystem/ClassTableFragment.java
@@ -0,0 +1,40 @@
+package com.kris.classsystem;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhuangfei.timetable.TimetableView;
+import com.zhuangfei.timetable.model.Schedule;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ClassTableFragment extends Fragment {
+
+    private TimetableView mtimetableView;
+    private List<Schedule> baby;
+    public ClassTableFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the fragment_start for this fragment
+        View view=inflater.inflate(R.layout.fragment_class_table, container, false);
+        mtimetableView = view.findViewById(R.id.id_timetableView);
+        mtimetableView.data(baby).curWeek(1).showView();
+        return view;
+    }
+
+    
+}
---
+++ app/src/main/java/com/kris/classsystem/StartFragment.java
@@ -0,0 +1,23 @@
+package com.kris.classsystem;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartFragment extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
