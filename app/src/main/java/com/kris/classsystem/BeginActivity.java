957e20313c6dc1acc6cafd9748c234ec9a60bc80
hullurent <2846886179@qq.com>
2019-06-26 22:46:54 +0800

    启动动画修补完善。

---
+++ app/src/main/java/com/kris/classsystem/BeginActivity.java
@@ -0,0 +1,17 @@
+package com.kris.classsystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(); // 无需布局渲染
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+
+    }
+}
